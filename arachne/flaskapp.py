8639b9403102adbc1b699c18900158220d4202e9
Daniel Masake Kitui <daniel.kitui@andela.com>
2018-05-10 12:44:12 +0300

    Refactor custom endpoints evaluation to a standalone function, for testing purposes

--- arachne/flaskapp.py
+++ arachne/flaskapp.py
@@ -2,9 +2,41 @@ import os
 import sys
 from flask import Flask
 from scrapy import version_info as SCRAPY_VERSION
-
 from arachne.exceptions import SettingsException, EndpointException
 from arachne.endpoints import list_spiders_endpoint, run_spider_endpoint
+from inspect import getmembers, isfunction, signature
+
+
+def endpoint_evaluator(endpoint):
+    """
+    Function to evaluate endpoint functions and and return the url and methods if they comply
+    :param endpoint: The endpoint function to be allowed.
+    :return: url and methods specified in the function
+    """
+    args = signature(endpoint).parameters
+
+    try:
+        url = args['url'].default
+    except KeyError:
+        raise EndpointException('Url for endpoint "{}" not supplied.'.format(endpoint.__name__))
+
+    if not isinstance(url, str):
+        raise EndpointException('Url for endpoint "{}" is not a string'.format(endpoint.__name__))
+
+    try:
+        methods = args['methods'].default
+    except KeyError:
+        # default method 'GET' is applied.
+        methods = ['GET']
+
+    allowed_methods = ['PUT', 'GET', 'POST', 'DELETE', 'PATCH']
+    for method in methods:
+        if method not in allowed_methods:
+            raise EndpointException('Supplied methods for "{}" endpoint is invalid.'
+                                    'Allowed methods are PUT, GET, POST, DELETE, PATCH'.format(endpoint.__name__))
+
+    return url, methods
+
 
 class Arachne(Flask):
 
@@ -33,7 +65,7 @@ class Arachne(Flask):
                        self.config['EXPORT_PATH'],
                        'json/')
         self.check_dir(self.config['LOGS'], self.config['LOGS_PATH'], '')
-        
+
         # from scrapy's version_info initialize Flask app
         # for version before 1.0.0 you don't need to init crawler_process
         if SCRAPY_VERSION >= (1, 0, 0):
@@ -77,7 +109,7 @@ class Arachne(Flask):
         """
         if config_name:
             cwd = os.getcwd()
-            export_dir = cwd+'/'+export_path+folder
+            export_dir = cwd + '/' + export_path + folder
             if not os.path.exists(export_dir):
                 raise SettingsException('Directory missing ', export_dir)
 
@@ -96,14 +128,15 @@ class Arachne(Flask):
         self.add_url_rule('/run-spider/<spider_name>', view_func=run_spider_endpoint)
         self.add_url_rule('/', view_func=list_spiders_endpoint)
 
-        self.custom_endpoints()
+        try:
+            import custom_endpoints as custom_endpoint_file
+        except ModuleNotFoundError:
+            # No custom endpoints file specified
+            return
 
+        self.add_custom_endpoints(custom_endpoint_file)
 
-    def _init_crawler_process(self):
-        from scrapy.crawler import CrawlerProcess
-        self.config['CRAWLER_PROCESS'] = CrawlerProcess()
-
-    def custom_endpoints(self):
+    def add_custom_endpoints(self, custom_endpoint_file):
         """
         method to add user defined endpoints if available. User defined endpoints are to be specified in a file
         called 'custom_endpoints.py' in the root directory. The method definations are to be in the format:
@@ -113,32 +146,14 @@ class Arachne(Flask):
             endpoint_method: list containing allowed methods. If none are provided, the endpoint method defaults to GET.
         :return: None.
         """
-        try:
-            import custom_endpoints
-        except ModuleNotFoundError:
-            # No custom endpoints file specified
-            return
 
-        endpoints = (fn for fn in getmembers(custom_endpoints) if isfunction(fn[1]) and fn[1].__module__ == custom_endpoints.__name__)
+        endpoints = (fn for fn in getmembers(custom_endpoint_file) if
+                     isfunction(fn[1]) and fn[1].__module__ == custom_endpoint_file.__name__)
         for endpoint in endpoints:
             if callable(endpoint[1]):
-                args = signature(endpoint[1]).parameters
-
-                try:
-                    url = args['url'].default
-                except KeyError:
-                    raise EndpointException('Urls for endpoint "{}" not supplied.'.format(endpoint[0]))
-
-                try:
-                    methods = args['methods'].default
-                except KeyError:
-                    # default method 'GET' is applied.
-                    methods = ['GET']
-
-                allowable_methods = ['PUT', 'GET', 'POST', 'DELETE', 'PATCH']
-                for method in methods:
-                    if method not in allowable_methods:
-                        raise EndpointException('Supplied methods for "{}" endpoint is invalid.'
-                                                'Allowed methods are PUT, GET, POST, DELETE, PATCH'.format(endpoint[0]))
-
+                url, methods = endpoint_evaluator(endpoint[1])
                 self.add_url_rule(url, view_func=endpoint[1], methods=methods)
+
+    def _init_crawler_process(self):
+        from scrapy.crawler import CrawlerProcess
+        self.config['CRAWLER_PROCESS'] = CrawlerProcess()
